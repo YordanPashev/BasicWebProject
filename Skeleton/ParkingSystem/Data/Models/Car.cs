c27ecef8de2039638f7b70edb14f7eaf85c0ed8d
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-12 23:46:28 +0300

    Improving readability in Models/Car.cs

--- Skeleton/ParkingSystem/Data/Models/Car.cs
+++ Skeleton/ParkingSystem/Data/Models/Car.cs
@@ -8,8 +8,10 @@ namespace ParkingSystem.Data.Models
     {
         [Required]
         public string CarMake { get; set; }
+
         [Required]
         public string PlateNumber { get; set; }
+
         public string ArivalTime { get; set; } = DateTime.Now.ToString(@"HH:mm dd.MM.yyyy");
 
     }
